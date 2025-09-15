2aba5585f411ba394ef40446e9efd1d8d34afe02
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-15 19:24:31 +0000

    fix: Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,7 +8,8 @@ const Landing: React.FC =  () => {
       <Card />
       <Button title="Small" styles=" border px-1 py-1 text-sm mt-3  bg-blue-500 text-white rounded-sm"/>
       <Button title="Medium" styles=" border px-2 py-1 text-md mt-3  bg-blue-500 text-white rounded-md"/>
-      <Button title="Large" styles=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full"/>
+      <Button title="Medium" styles=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-lg"/>
+      <Button title="Large" styles=" border px-4 py-1 text-xl mt-3  bg-blue-500 text-white rounded-full"/>
     </div>
   )
 }
